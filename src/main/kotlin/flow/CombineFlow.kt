090e34e5064e17c974c24cf1c6067459635267c1
ThirdPrince <18796017665@163.com>
2026-02-24 19:38:59 +0800

    add combine test

--- src/main/kotlin/flow/CombineFlow.kt
+++ src/main/kotlin/flow/CombineFlow.kt
@@ -7,18 +7,20 @@ import utils.log
 
 val isAvailable = MutableStateFlow(1)
 
-fun isAvailableFlow(): Flow<Int> = isAvailable.asStateFlow()
+fun isAvailableFlow(): Flow<Int> = flow {
+    suspend { }
+}
 
 
 fun main() = runBlocking {
     val flow2 = MutableStateFlow(2)
-    val flow: StateFlow<Int> = combine(flow2,isAvailableFlow()) { isAvailableFlow, flow2 ->
+    val uiState: StateFlow<Int> = combine(flow2, isAvailableFlow()) { isAvailableFlow, flow2 ->
         isAvailableFlow + flow2
     }.stateIn(
         this, started = SharingStarted.WhileSubscribed(), -1
     )
     launch {
-        flow.collect {
+        uiState.collect {
             log("it = $it")
         }
     }
